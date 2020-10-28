af54201a8688114da01b91d886cdb119e3b21102
Michael <michaelmckn4650@gmail.com>
2020-10-28 01:52:01 +0000

    Add files via upload

---
+++ Part2/Phonebook/src/components/Notification.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Notification = ({message, styleType}) => {
+
+  
+
+    if (message === null)
+    return null
+  
+    
+
+return (
+    <div style={styleType}>
+    {message}
+    </div>
+)
+
+}
+
+export default Notification
